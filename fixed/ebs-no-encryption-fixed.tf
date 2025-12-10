53a035ccfe0cdee4a48ce132b7778d0575f295be
D-Lomax-Jr <darryllomax68@gmail.com>
2025-12-10 02:04:28 -0500

    Implement KMS encryption for EBS volume
    Added KMS key resource for EBS volume encryption and updated EBS volume to use encryption.

--- fixed/ebs-no-encryption-fixed.tf
+++ fixed/ebs-no-encryption-fixed.tf
@@ -1,4 +1,38 @@
-resource "aws_ebs_volume" "unencrypted" {
+# KMS key for EBS encryption
+resource "aws_kms_key" "ebs_kms" {
+  description         = "CMK for EBS volume encryption"
+  enable_key_rotation = true
+
+  # Minimal, non-wildcard principal policy
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid      = "EnableRootPermissions"
+        Effect   = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::123456789012:root"
+        }
+        Action   = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Secure, KMS-encrypted EBS volume
+resource "aws_ebs_volume" "secure_volume" {
   availability_zone = "us-east-1a"
-  size = 8
+  size              = 8
+  encrypted         = true
+  kms_key_id        = aws_kms_key.ebs_kms.arn
+
+  tags = {
+    Name = "secure-ebs-volume"
+  }
 }
