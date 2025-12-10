7f201ec3f9771887e64b50b5c215bf654829b2a0
Darryl Lomax <low8maximum@KaliBox.Roma>
2025-12-09 19:49:15 -0500

    Revert "updated 'fixed' files"
    This reverts commit 0e3db21118ee920963369cb3bfdc333a83d6aad1.

--- fixed/iam-wildcard-fixed.tf
+++ fixed/iam-wildcard-fixed.tf
@@ -1,27 +1,11 @@
-# Least-privilege IAM policy: read-only access to a specific S3 bucket
-resource "aws_iam_policy" "limited_read_only" {
-  name        = "limited-readonly-policy"
-  description = "Read-only access to a single logs bucket"
-
+resource "aws_iam_policy" "wild" {
+  name = "wild-policy"
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Sid    = "AllowListBucket"
-        Effect = "Allow"
-        Action = [
-          "s3:ListBucket"
-        ]
-        Resource = "arn:aws:s3:::app-logs-bucket"
-      },
-      {
-        Sid    = "AllowGetObjects"
-        Effect = "Allow"
-        Action = [
-          "s3:GetObject"
-        ]
-        Resource = "arn:aws:s3:::app-logs-bucket/*"
-      }
-    ]
+    Statement = [{
+      Effect = "Allow"
+      Action = "*"
+      Resource = "*"
+    }]
   })
 }
