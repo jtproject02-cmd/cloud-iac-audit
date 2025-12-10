0e3db21118ee920963369cb3bfdc333a83d6aad1
Darryl Lomax <low8maximum@KaliBox.Roma>
2025-12-09 19:26:08 -0500

    updated 'fixed' files

--- fixed/iam-wildcard-fixed.tf
+++ fixed/iam-wildcard-fixed.tf
@@ -1,11 +1,27 @@
-resource "aws_iam_policy" "wild" {
-  name = "wild-policy"
+# Least-privilege IAM policy: read-only access to a specific S3 bucket
+resource "aws_iam_policy" "limited_read_only" {
+  name        = "limited-readonly-policy"
+  description = "Read-only access to a single logs bucket"
+
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Action = "*"
-      Resource = "*"
-    }]
+    Statement = [
+      {
+        Sid    = "AllowListBucket"
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket"
+        ]
+        Resource = "arn:aws:s3:::app-logs-bucket"
+      },
+      {
+        Sid    = "AllowGetObjects"
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject"
+        ]
+        Resource = "arn:aws:s3:::app-logs-bucket/*"
+      }
+    ]
   })
 }
--- fixed/kms-no-rotation-fixed.tf
+++ fixed/kms-no-rotation-fixed.tf
@@ -1,4 +1,25 @@
-resource "aws_kms_key" "no_rotate" {
-  description          = "bad kms key"
-  enable_key_rotation  = false
+resource "aws_kms_key" "secure_kms" {
+  description         = "Secure KMS key with rotation and explicit policy"
+  enable_key_rotation = true
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowRootAccount"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::123456789012:root"
+        }
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
